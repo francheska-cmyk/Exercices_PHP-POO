69b34d08fa8c5d3a32ab60b9494a20ec4b1ea335
francheska <charlotfrancheska@gmail.com>
2026-08-19 14:24:11 +0200

    "new branch_exo"

--- model/modelArticle.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getArticle($bdd){
-    //1. Préparer une requête pour SELECT les utilisateurs
-    $request = $bdd->prepare('SELECT title, pseudo FROM article INNER JOIN user ON user.id = article.user_id');
-   //2.  Exécution
-    $request->execute();
-
-    return $request->fetchAll(PDO::FETCH_ASSOC);
-}
